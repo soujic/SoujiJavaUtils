9b49352022d461415a2a59ca5115417073c4b973
Soujanya Chichula <souji@advancedemedia.com>
2015-12-25 12:13:59 -0800

    List added

---
+++ src/main/java/com/soujijavautils/arrayimplementation/collection/ArrayList.java
@@ -0,0 +1,9 @@
+package com.soujijavautils.arrayimplementation.collection;
+
+import com.soujijavautils.collection.List;
+
+/**
+ * Created by soujanya on 12/25/15.
+ */
+public class ArrayList implements List{
+}
---
+++ src/test/java/com/soujijavautils/arrayimplementation/collection/ArrayListTest.java
@@ -0,0 +1,10 @@
+package com.soujijavautils.arrayimplementation.collection;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by soujanya on 12/25/15.
+ */
+public class ArrayListTest extends TestCase {
+
+}
